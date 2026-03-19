4327d59dcdbabf7d4be2ea6e6af836ee8ca9c1fe
nikiborf-sketch <nikibo.rf@gmail.com>
2026-03-19 19:50:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json({ limit: "10mb" }));
+
 app.get("/", (req, res) => {
   res.send("Kalorio API работает 🚀");
+});
+
+app.post("/analyze", async (req, res) => {
+  const image = req.body.image;
+
+  // пока просто тест
+  res.json({
+    food: "Паста",
+    calories: 520
+  });
 });
 
 app.listen(3000, () => {
